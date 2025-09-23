8939e9393d83e1ac7ea8c9fc33c00edb6b5f32da
congtruqn <congtruqn@gmail.com>
2025-09-23 13:45:50 +0700

    update

--- src/api/apiClient.ts
+++ src/api/apiClient.ts
@@ -1,10 +1,22 @@
 import axios from 'axios';
-
+import { Cookies } from "react-cookie";  
+const cookies = new Cookies();  
+const accessToken = cookies.get("accessToken");  
 const apiClient = axios.create({
-  baseURL: 'http://localhost:3001',
+  baseURL: 'http://develop.softnests.com/api',
   headers: {
     'Content-Type': 'application/json',
   },
 });
-
+apiClient.interceptors.request.use(
+  config => {
+    if (accessToken) {
+      config.headers['Authorization'] = 'Bearer ' + accessToken
+    }
+    return config
+  },
+  error => {
+    Promise.reject(error)
+  }
+)
 export default apiClient
--- src/pages/Customers.tsx
+++ src/pages/Customers.tsx
@@ -1,77 +1,69 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Table } from 'antd';
 import type { TableColumnsType, TableProps } from 'antd';
+import apiClient from "../api/apiClient";
 
 interface DataType {
     key: React.Key;
     name: string;
-    age: number;
-    address: string;
+    price: number;
+    parent_name: string;
 }
-const data: DataType[] = [
-    {
-      key: '1',
-      name: 'John Brown',
-      age: 32,
-      address: 'New York No. 1 Lake Park',
-    },
-    {
-      key: '2',
-      name: 'Jim Green',
-      age: 42,
-      address: 'London No. 1 Lake Park',
-    },
-    {
-      key: '3',
-      name: 'Joe Black',
-      age: 32,
-      address: 'Sydney No. 1 Lake Park',
-    },
-    {
-      key: '4',
-      name: 'Jim Red',
-      age: 32,
-      address: 'London No. 2 Lake Park',
-    },
-];
 const columns: TableColumnsType<DataType> = [
     {
       title: 'Name',
       dataIndex: 'name',
-      filterMode: 'tree',
-      filterSearch: true,
-      onFilter: (value, record) => record.name.includes(value as string),
-      width: '30%',
     },
     {
       title: 'Age',
-      dataIndex: 'age',
-      sorter: (a, b) => a.age - b.age,
+      dataIndex: 'price',
     },
     {
       title: 'Address',
-      dataIndex: 'address',
-      onFilter: (value, record) => record.address.startsWith(value as string),
-      filterSearch: true,
-      width: '40%',
+      dataIndex: 'parent_name',
     },
   ];
+
+const Products: React.FC = ()=>{  
+  const [data, setData] = useState([]);
+  const [total, setTotal] = useState(0);
+  const [pageSize, setPageSize] = useState(10);
+  const fetchData = async (pageSize: number, pageNumber: number) => {
+    try {
+        const response = await apiClient.get('/products?pageSize='+pageSize+'&pageNumber='+pageNumber); // Replace with your actual API endpoin
+        const temp = response.data?.data.map((item: { _id: unknown; detail: { name: unknown; }[]; }) => {
+          return {
+            key:  item._id,
+            name: item?.detail[0]?.name,
+            price: 32,
+            parent_name: 'New York No. 1 Lake Park',
+          }
+        })
+        setData(temp);
+        setTotal(response.data?.count)
+    } catch (err) {
+      console.log(err);
+    }
+  };
   const onChange: TableProps<DataType>['onChange'] = (pagination, filters, sorter, extra) => {
     console.log('params', pagination, filters, sorter, extra);
+    fetchData(pagination.pageSize || 0, pagination.current || 0);
+    setPageSize(pagination.pageSize || 10);
   }
-const Customers: React.FC = ()=>{  
-
+  useEffect(() => {
+    fetchData(10,1);
+  }, []);
   return (
     <section>
-      <h2 className="title">{"Khách hàng 1"}</h2>
+      <h2 className="title">{"Danh sách sản phẩm"}</h2>
       <Table<DataType> columns={columns} dataSource={data} onChange={onChange}  pagination={{
-        pageSize: 10,
+        pageSize: pageSize,
         showSizeChanger: true,
-        total: 100,
+        total: total,
         pageSizeOptions: ['10', '20', '50'],
     }}/>
     </section>
   );
 }
 
-export default Customers;
+export default Products;
