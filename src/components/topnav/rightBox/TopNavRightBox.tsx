07abaf15fd52d310428101b3ab14cc66db82a365
congtruqn <congtruqn@gmail.com>
2025-10-02 21:37:43 +0700

    update

--- src/components/topnav/rightBox/TopNavRightBox.tsx
+++ src/components/topnav/rightBox/TopNavRightBox.tsx
@@ -4,15 +4,25 @@ import Profile from "./profile/Profile";
 import classes from "./TopNavRightBox.module.scss";
 import { useNotiStore } from "../../../store/notiStore";
 import { Link } from "react-router-dom";
+import apiClient from "../../../api/apiClient";
 
 function TopNavRightBox() {
   const value = useNotiStore((state ) => state.value);
+  const { setValue } = useNotiStore();
+  const updateNoti = async () => {
+    try {
+        await apiClient.put('notifications', {});
+        setValue(0);
+    } catch (err) {
+      console.error('Fetch error: ', err);
+    } finally { /* empty */ }
+  };
   return (
     <div className={classes.topNavBox_right}>
       <div className={classes.wrapper}>
       <div className={classes.lang}>
         <div className="notification-container">
-        <Link to={"/crm/notifications"}><Icon icon="iconamoon:notification-thin" width="25" /></Link>
+        <Link to={"/crm/notifications"} onClick={updateNoti}><Icon icon="iconamoon:notification-thin" width="25" /></Link>
           <span className="notification-counter">{value}</span>
         </div>
       </div>
