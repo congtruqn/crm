dafc40189ed8f15a4558a37ce9c7911f39003b52
congtruqn <congtruqn@gmail.com>
2025-10-09 15:57:49 +0700

    update

--- src/pages/Works.tsx
+++ src/pages/Works.tsx
@@ -106,7 +106,7 @@ const Works: React.FC = ()=>{
           return {
             key:  item._id,
             id: item._id,
-            count: ((current -1) * pageSize) + index + 1,
+            count: ((pageNumber -1) * pageSize) + index + 1,
             customer: item.customer,
             event_type: item.event_type,
             user: item.user,
