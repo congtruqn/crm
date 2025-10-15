21e205cd7a633ddd4bef6823b040504ad1f91774
congtruqn <congtruqn@gmail.com>
2025-10-15 17:41:51 +0700

    update

--- src/components/quote/viewQuote.tsx
+++ src/components/quote/viewQuote.tsx
@@ -1,19 +1,20 @@
 import React, { useEffect, useRef, useState } from "react";
-import apiClient from "../../api/apiClient";
-import type { Quote } from "../../interfaces/quote";
 import jsPDF from 'jspdf';
 import html2canvas from 'html2canvas';
+import { useMyStore } from "../../store/userStore";
+import type { User } from "../../interfaces/user";
 interface MyComponentProps {
   quoteId: string,
 }
 
 const ViewCustomer: React.FC<MyComponentProps> = ({ quoteId }: MyComponentProps)=>{
   const [htmlContent, setHtmlContent] = useState('');
+  const value:User = useMyStore((state ) => state.value);
   const contentRef = useRef(null);
   useEffect(() => {
     const fetchHtml = async () => {
       try {
-        const response = await fetch(`${import.meta.env.VITE_BASE_URL}`+'/view-quote');
+        const response = await fetch(`${import.meta.env.VITE_BASE_URL}`+'/view-quote?customer_id=+'+value.customer_id+'+&quoteId='+quoteId);
         if (!response.ok) {
           throw new Error(`HTTP error! status: ${response.status}`);
         }
@@ -27,32 +28,6 @@ const ViewCustomer: React.FC<MyComponentProps> = ({ quoteId }: MyComponentProps)
 
     fetchHtml();
   }, []);
-  const [data, setData] = useState<Quote>({
-        _id: "",
-        customer: "",
-        customerId: "",
-        create_date: "",
-        amount: 0,
-        text_amount: "",
-        user: "",
-        items:[],
-        invoiceNumber: "",
-  });
-  const getCustomerInfo = async (customerId: string): Promise<void> => {
-    try {
-        const response = await apiClient.get('/quote/'+customerId); // Replace with your actual API endpoin
-        const temp = response.data;
-        setData(temp)
-    } catch (err) {
-      console.log(err);
-    }
-  };
-  useEffect(() => {
-    if(quoteId){
-      getCustomerInfo(quoteId);
-    }
-  // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [quoteId]);
   const generatePdf = async () => {
     if (contentRef.current) {
       const canvas = await html2canvas(contentRef.current);
@@ -81,27 +56,6 @@ const ViewCustomer: React.FC<MyComponentProps> = ({ quoteId }: MyComponentProps)
     <>
     <button onClick={generatePdf}>Generate PDF</button>
     <div ref={contentRef} dangerouslySetInnerHTML={{ __html: htmlContent }} />
-      <div className="row">
-
-        <div className="form-group col-sm-6">
-          <label className="col-sm-12 control-label">
-            Tên khách hàng : <strong>{data?.customer}</strong>
-          </label>
-        </div>
-
-        <div className="form-group col-sm-6">
-          <label className="col-sm-12 control-label">
-            Nhu cầu: <strong>{data?.amount}</strong>
-          </label>
-        </div>
-
-        <div className="form-group col-sm-6">
-          <label className="col-sm-12 control-label">
-            Số điện thoại: <strong>{data?.text_amount}</strong>
-          </label>
-        </div>
-        <div className="clear"></div>
-      </div>
     </>
   );
 }
